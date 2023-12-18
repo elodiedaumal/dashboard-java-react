01347cafee46fa4575dd3e1b2b3befea0eab65ac
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-12-18 10:58:36 +0100

    initializing Student fields

---
+++ react-springboot-dashboard/src/main/java/com/javaproject/reactspringbootdashboard/ReactSpringbootDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.reactspringbootdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootDashboardApplication.class, args);
+	}
+
+}
---
+++ react-springboot-dashboard/src/test/java/com/javaproject/reactspringbootdashboard/ReactSpringbootDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.reactspringbootdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
